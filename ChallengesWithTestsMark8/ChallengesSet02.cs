bc360af6aca87558c27c30a32713d4600825911d
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-28 16:20:06 -0500

    Challenge Set 02 Completed, but not checked

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,55 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet02
     {
+
+        // *** Check for null values first ***
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return (vals.Length % 2 == 0) ? true : false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return (number % 2 == 0) ? true : false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return (num % 2 != 0) ? true : false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() <= 0)
+                return 0;
+            return Enumerable.Max(numbers) + Enumerable.Min(numbers);
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str1.Length;
+            }
         }
 
-        public int Sum(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
+        public int Sum(int[] numbers) => (numbers == null) ? 0 : Enumerable.Sum(numbers);
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length <= 0)
+            {
+                return 0;
+            }
+
+            var evensOnly = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0 || numbers[i] % 2 == 0)
+                {
+                    evensOnly[i] = numbers[i];
+                }
+            }
+            return Enumerable.Sum(evensOnly);
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0) return false;
+
+            int listSum = Enumerable.Sum(numbers);
+
+            if (listSum % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
+        //******************
         public long CountOfPositiveOddsBelowNumber(long number)
         {
             throw new NotImplementedException();
